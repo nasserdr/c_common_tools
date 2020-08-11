4063d2f1d6525aacf3443abb50a7662fd91ca7bc
nasserdr <hassan.nasser@me.com>
2020-08-11 09:57:46 +0000

    file 101 added

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,33 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int a, b, c;
+
+	for (a = 0; a <= 8; a++)
+		for (b = a; b <= 9; b++)
+			for (c = b; c <=9; c++)
+	{
+		if ( a!= b && b != c)
+		{
+			putchar(a + '0');
+			putchar(b + '0');
+			putchar(c + '0');
+			if(!(a == 7 && b == 8 && c == 9))
+			{
+			putchar(',');
+			putchar(' ');
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
+}
