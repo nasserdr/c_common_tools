0cc782e4d5d6fca7370110c301e4df8e31fd8737
nasserdr <hassan.nasser@me.com>
2020-08-11 07:07:13 +0000

    file 5 added

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,15 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/*
+ *A program that prints the alphabet in lowercase, followed by a new line.
+ */
+
+int main(void)
+{
+	int a;
+	for ( a = 0; a <= 9; a++)
+		printf("%d",a);
+	printf("\n");
+	return (0);
+}
