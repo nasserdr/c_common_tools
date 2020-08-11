08a3fc982b0d2f8b94d00af460a1b6eb98059ef3
nasserdr <hassan.nasser@me.com>
2020-08-11 10:00:55 +0000

    file 101 added

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -14,20 +14,20 @@ int main(void)
 
 	for (a = 0; a <= 8; a++)
 		for (b = a; b <= 9; b++)
-			for (c = b; c <=9; c++)
-	{
-		if ( a!= b && b != c)
-		{
-			putchar(a + '0');
-			putchar(b + '0');
-			putchar(c + '0');
-			if(!(a == 7 && b == 8 && c == 9))
+			for (c = b; c <= 9; c++)
 			{
-			putchar(',');
-			putchar(' ');
+				if (a != b && b != c)
+				{
+					putchar(a + '0');
+					putchar(b + '0');
+					putchar(c + '0');
+					if (!(a == 7 && b == 8 && c == 9))
+					{
+						putchar(',');
+						putchar(' ');
+					}
+				}
 			}
-		}
-	}
 	putchar('\n');
 	return (0);
 }
