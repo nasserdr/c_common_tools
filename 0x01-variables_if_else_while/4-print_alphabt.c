bf14da0c13701db41bb8bf8e8db91ad84442a475
nasserdr <hassan.nasser@me.com>
2020-08-11 07:04:34 +0000

    file 4 added

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,20 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/*
+ *A program that prints the alphabet in lowercase, followed by a new line.
+ */
+
+int main(void)
+{
+	char a;
+	for ( a = 'a'; a <= 'z'; a++)
+	{
+		if( !(a=='e' || a=='q'))
+		{
+			putchar(a);
+		}
+	}
+	putchar('\n');
+	return (0);
+}
