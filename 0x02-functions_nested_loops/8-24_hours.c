7069a6b762d28d70b8c571cfc3cb76c66ed159d7
nasserdr <hassan.nasser@me.com>
2020-08-13 15:21:15 +0000

    file 8 added

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,26 @@
+#include "holberton.h"
+
+/**
+ * jack_bauer - compute the absolute value of a number
+ *
+ * Return: Nothing.
+ */
+
+void jack_bauer(void)
+{
+
+	int i, j, k, l;
+
+	for (i = 0; i <= 2; i++)
+		for (j = 0; j <= 3; j++)
+			for (k = 0; k <= 5; k++)
+				for (l = 0; l <= 9; l++)
+				{
+					_putchar(i + '0');
+					_putchar(j + '0');
+					_putchar(':');
+					_putchar(k + '0');
+					_putchar(l + '0');
+					_putchar('\n');
+				}
+}
