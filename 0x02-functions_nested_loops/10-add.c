e4584aec0e2fdb98a3634d4286e4564dff493777
nasserdr <hassan.nasser@me.com>
2020-08-13 17:48:17 +0000

    File 10 added

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,15 @@
+#include "holberton.h"
+
+/**
+ * add - Adds 2 numbers
+ *
+ * @m1: input integer 1
+ * @m2: input integer 2
+ *
+ * Return: m1 + m2
+ */
+
+int add(int m1, int m2)
+{
+	return (m1 + m2);
+}
