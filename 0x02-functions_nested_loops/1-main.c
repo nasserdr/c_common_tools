51d1cd147f3c50553a9c562fc0a5a5c65d612dc7
nasserdr <hassan.nasser@me.com>
2020-08-13 11:07:28 +0000

    file 1 added

---
+++ 0x02-functions_nested_loops/1-main.c
@@ -0,0 +1,12 @@
+#include "holberton.h"
+
+void print_alphabet(void)
+{
+        char a;
+
+        for ( a = 'a'; a <= 'z'; a++)
+        {
+                _putchar(a);
+        }
+	_putchar('\n');
+}
