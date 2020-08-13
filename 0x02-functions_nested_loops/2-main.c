a0726a04fefdb73bf9a96294e8bcd7e7774b3794
nasserdr <hassan.nasser@me.com>
2020-08-13 11:21:10 +0000

    File 2 added

---
+++ 0x02-functions_nested_loops/2-main.c
@@ -0,0 +1,13 @@
+#include "holberton.h"
+
+/**
+ * main - Entry point. Prints alphabet 10 times.
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	print_alphabet_x10();
+	return (0);
+}
