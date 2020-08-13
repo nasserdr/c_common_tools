5ac170038cb15a97d0228330ea2c3b518c95634c
nasserdr <hassan.nasser@me.com>
2020-08-13 17:54:40 +0000

    file 11 finished

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,20 @@
+#include "holberton.h"
+#include <stdio.h>
+
+/**
+ * print_to_98 - Prints all numbers from n to 98
+ *
+ * @c: input integer from which we wish to start the counting
+ * Return: Nothing
+ */
+
+void print_to_98(int c)
+{
+        int i;
+
+	for (i = c; i < 98; i++)
+	{
+		printf("%d, ", i);
+	}
+	printf("98 \n");
+}
