29be1e1e0baf8d2799f3b24b407025bb189b8ea4
nasserdr <hassan.nasser@me.com>
2020-08-13 13:43:26 +0000

    file 4 fixed

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -1,18 +1,18 @@
 #include "holberton.h"
 
 /**
- * _isalpha - Checks whether the argument is alphanumeric
+ * _isalpha - Checks whether the argument is alphabetic
  *
  * @c: input character
  *
  * Return:
- * 1 if @c is alphanumeric
+ * 1 if @c is alphabetic
  * 0 otherwise
  */
 
 int _isalpha(int c)
 {
-	if (isalpha(c))
+	if ((c <= 'z' && c >= 'a') || (c <= 'Z' && c >= 'A'))
 		return (1);
 	else
 		return (0);
