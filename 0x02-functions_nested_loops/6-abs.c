ac944be0cb25ae3c404c167344c2d56e1d1e7727
nasserdr <hassan.nasser@me.com>
2020-08-13 14:46:17 +0000

    6 added

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,19 @@
+#include "holberton.h"
+
+/**
+ * _abs - compute the absolute value of a number
+ *
+ * @c: input character
+ *
+ * Return:
+ * c if @c is bigger than 0
+ * -c otherwise
+ */
+
+int _abs(int c)
+{
+	if (c > 0)
+		return (c);
+	else
+		return (-1*c);
+}
