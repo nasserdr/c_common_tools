da0d183bfeee5490d8e16fc65dcd21489bb75d40
nasserdr <hassan.nasser@me.com>
2020-08-13 14:52:33 +0000

    7 added

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,15 @@
+#include "holberton.h"
+
+/**
+ * _print_last_digit - compute the absolute value of a number
+ *
+ * @c: input character
+ *
+ * Return:
+ * The last digit of the number
+ */
+
+int _print_last_digit(int c)
+{
+	return c % 10;
+}
