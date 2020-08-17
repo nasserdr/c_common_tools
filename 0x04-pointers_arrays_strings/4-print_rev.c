8588701548aad38dbc2c46deaab21363b934efc2
nasserdr <hassan.nasser@me.com>
2020-08-17 17:38:16 +0000

    File 4 added

---
+++ 0x04-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,24 @@
+#include "holberton.h"
+#include <stdio.h>
+#include <string.h>
+
+/**
+ * print_rev -  Prints the string s in reverse
+ *
+ * @s: pointer to a string
+ *
+ * Return: Nothing
+ */
+
+void print_rev(char *s)
+{
+	int n;
+
+	n = strlen(s) - 2;
+	while (n != 1)
+	{
+		_putchar(*(s + n));
+		n--;
+	}
+	_putchar('\n');
+}
