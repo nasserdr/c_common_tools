bac98e0dc71e1273c9981f7cfdf8d1d2c7949b89
nasserdr <hassan.nasser@me.com>
2020-08-17 19:18:49 +0000

    File 6 pushed

---
+++ 0x04-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,29 @@
+#include "holberton.h"
+#include <stdio.h>
+
+/**
+ * puts2 -  Prints every other character the string s
+ *
+ * @s: pointer to a string
+ *
+ * Return: Nothing
+ */
+
+void puts2(char *s)
+{
+	int n;
+
+  	n = 0;
+
+	while (*(s + n) != '\0')
+	{
+		if ( (n % 2) == 0)
+		{
+		_putchar(*(s + n));
+		}
+		n++;
+
+	}
+	_putchar('\n');
+
+}
