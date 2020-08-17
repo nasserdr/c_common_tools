a5940b02e2029d7713d17d69832d9042014e6204
nasserdr <hassan.nasser@me.com>
2020-08-17 13:52:35 +0000

    Folder 0x04 added

---
+++ 0x04-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,14 @@
+#include "holberton.h"
+
+/**
+ * reset_to_98 - Update the value of the pointer to 98
+ *
+ * @*n: Pointer to an integer
+ *
+ * Return: Nothing
+ */
+
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
