25c97b3852233fa3b82696b971bdb6b0fc88f36a
nasserdr <hassan.nasser@me.com>
2020-08-18 17:01:02 +0000

    File 5 added

---
+++ 0x05-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,24 @@
+#include "holberton.h"
+#include <stdio.h>
+
+/**
+ * string_toupper - Change all letters to capital letters
+ *
+ * @s: the string
+ *
+ * Return: the string in uppercase
+ */
+
+char *string_toupper(char *s)
+{
+	int i;
+
+	i = 0;
+	while (*(s + i) != '\0')
+	{
+		if (*(s + i) >= 'a' && *(s + i) <= 'z')
+			*(s + i) = *(s + i) - 32;
+	}
+
+	return (s);
+}
