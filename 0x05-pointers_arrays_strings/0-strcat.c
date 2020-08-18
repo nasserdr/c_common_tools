c8674f686a933ab8504821e84a1afbb7df6127d7
nasserdr <hassan.nasser@me.com>
2020-08-18 15:49:32 +0000

    Folder 0x05 added

---
+++ 0x05-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,25 @@
+#include "holberton.h"
+#include <stdio.h>
+
+/**
+ * _strcpy -  Copies the string src into the string dest
+ *
+ * @src: the source string
+ * @dest: The destination string
+ *
+ * Return: Nothing
+ */
+
+char *_strcpy(char *dest, char *src)
+{
+	int i;
+
+	i = 0;
+	while (*(src + i) != '\0')
+	{
+		dest[i] = *(src + i);
+		i++;
+	}
+	dest[i] = '\0';
+	return (dest);
+}
---
+++ 0x05-pointers_arrays_strings/README.md~
@@ -0,0 +1,11 @@
+0x03-debugging  
+**Author: Hassan Nasser**
+
+**Description:**
+This folder contains the exercises of the 0x03-debugging:  
+
+The list of files is:  
+**0-main.c, holberton.h**: Exercise #1.  
+**1-main.c**: Exercise #2.  
+**2-largest_number.c, holberton.h**: Exercise #3.  
+**3-print_remaining_days.c, holberton.h**: Exercise #4.  
