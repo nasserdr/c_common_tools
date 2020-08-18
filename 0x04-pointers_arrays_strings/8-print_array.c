5880e6f2440c196f75bce3d156a31191baa5a15a
nasserdr <hassan.nasser@me.com>
2020-08-18 13:51:37 +0000

    file 8 finalized

--- 0x04-pointers_arrays_strings/8-print_array.c
+++ 0x04-pointers_arrays_strings/8-print_array.c
@@ -14,9 +14,12 @@ void print_array(int *a, int n)
 {
 	int i;
 
-	for (i = 0; i < n - 1; i++)
+	if (n >0 )
 	{
-		printf("%d, ", a[i]);
+		for (i = 0; i < n - 1; i++)
+		{
+			printf("%d, ", a[i]);
+		}
+		printf("%d\n", a[n - 1]);
 	}
-	printf("%d\n", a[n - 1]);
 }
