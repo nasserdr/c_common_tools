9ea21620346988a466e0c8b88ec9093691117f06
nasserdr <hassan.nasser@me.com>
2020-08-18 13:45:23 +0000

    file 8 finalized

--- 0x04-pointers_arrays_strings/8-print_array.c
+++ 0x04-pointers_arrays_strings/8-print_array.c
@@ -14,9 +14,9 @@ void print_array(int *a, int n)
 {
 	int i;
 
-	for (i = 0; i < n-1; i++)
+	for (i = 0; i < n - 1; i++)
 	{
 		printf("%d ,", a[i]);
 	}
-	printf("%d\n", a[n-1]);
+	printf("%d\n", a[n - 1]);
 }
