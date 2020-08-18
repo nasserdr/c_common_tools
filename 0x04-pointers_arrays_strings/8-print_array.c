f453a579e861cb73d7cf4332069ebb02fa8a4acb
nasserdr <hassan.nasser@me.com>
2020-08-18 13:43:55 +0000

    file 8 finalized

---
+++ 0x04-pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,22 @@
+#include "holberton.h"
+#include <stdio.h>
+
+/**
+ * print_array -  Prints the first n elements of an array
+ *
+ * @a: Array
+ * @n: Number of element
+ *
+ * Return: Nothing
+ */
+
+void print_array(int *a, int n)
+{
+	int i;
+
+	for (i = 0; i < n-1; i++)
+	{
+		printf("%d ,", a[i]);
+	}
+	printf("%d\n", a[n-1]);
+}
