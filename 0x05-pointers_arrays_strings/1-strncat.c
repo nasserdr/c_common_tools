706619ae8eda5866d78fec772eb0bf8bf2b33315
nasserdr <hassan.nasser@me.com>
2020-08-18 16:13:38 +0000

    File 1 strncat added

---
+++ 0x05-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,35 @@
+#include "holberton.h"
+#include <stdio.h>
+
+/**
+ * _strncat -  Concatenates n characters from src at the end of dest
+ *
+ * @src: the source string
+ * @dest: The destination string
+ * @n: The number of characters to be concatenated
+ *
+ * Return: the destination string dest
+ */
+
+char *_strncat(char *dest, char *src, int n)
+{
+	int i, len, n_copied;
+
+	i = 0;
+	while (*(dest + i) != '\0')
+	{
+		i++;
+	}
+
+	len = i;
+	i = 0;
+	n_copied = 0;
+	while (*(src + i) != '\0' && n_copied <= n)
+	{
+		dest[len + i] = src[i];
+		i++;
+		n_copied++;
+	}
+	dest[len + i] = '\0';
+	return (dest);
+}
