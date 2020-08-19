9f286d7a7bde84e3318a96b72adb5abc4d0fd0f2
nasserdr <hassan.nasser@me.com>
2020-08-19 11:19:16 +0000

    File 7 added

---
+++ 0x06-pointers_arrays_strings/7-print_chessboard.c
@@ -0,0 +1,25 @@
+#include "holberton.h"
+#include <stdio.h>
+
+/**
+ * print_chessboard - Prints the chessboard
+ *
+ * @a: pointer to the chessboard
+ *
+ * Return: nothing
+ */
+
+void print_chessboard(char (*a)[8])
+{
+	int i, j;
+
+	for (i = 0; i < 8; i++)
+	{
+		for (j = 0; j < 8; j++)
+		{
+			_putchar(a[i][j]);
+		}
+		_putchar('\n');
+	}
+
+}
